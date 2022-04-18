d8aa6d84dd8a54f7ce8401c89d6f351b69811052
Mishad5 <abdullahahmedkhan100@gmail.com>
2022-04-18 17:27:48 +0600

    redirect fixed

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,43 +1,54 @@
 import { Button } from 'bootstrap';
 import React, { useRef } from 'react';
 import { Form } from 'react-bootstrap';
+import checkout from '../../images/checkout.jpg';
 
 const Checkout = () => {
     const handleSubmit = e => {
         e.preventDefault();
     }
     const emailRef = useRef('');
+    const nameRef = useRef('');
+    const contactRef = useRef('');
+    const addressRef = useRef('');
     return (
-        <div style={{minHeight:"100vh"}} className="container">
+        <div style={{ minHeight: "100vh" }} className="container">
             <h2>Checkout</h2>
-            <form onSubmit={handleSubmit} style={{maxWidth:"500px"}} className='mx-auto border border-2 p-3 rounded shadow'>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Your Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} required/>
-                        
+            <div className='d-flex flex-sm-row'>
+                <div>
+                    <img src={checkout} alt="" width='500' />
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Service Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} required/>
-                        
+                <div className='w-75'>
+                    <form onSubmit={handleSubmit} style={{}} className='border border-2 p-3 rounded shadow bg-light'>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Your Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" ref={nameRef} required />
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Service Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" ref={emailRef} required />
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Contact No</label>
+                            <input type="number" class="form-control" id="exampleInputEmail1" ref={contactRef} required />
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Your Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" ref={emailRef} required />
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Your Address</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" ref={addressRef} required />
+
+                        </div>
+                        <button type="submit" class="btn btn-primary w-100">Place Order</button>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Contact No</label>
-                    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} required/>
-                        
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Your Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} required/>
-                        
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Your Address</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} required/>
-                        
-                </div>
-                <button type="submit" class="btn btn-primary w-100">Proceed Checkout</button>
-            </form>
+            </div>
         </div>
     );
 };
